e896684fb9ca72caebb2202bf52fc5e0094c7239
shabir pjr <halemwagih6@gmail.com>
2023-06-02 23:54:21 +0300

    + finish shared preferences and finish isDone

---
+++ lib/storge local/cash_Helper.dart
@@ -0,0 +1,7 @@
+import 'package:shared_preferences/shared_preferences.dart';
+class SharedPreferences{
+  static late SharedPreferences prefs;
+  static void saveLanguageSharedPreferences(String saveLanguage)async{
+    var prefs = await SharedPreferences.;
+  }
+}
